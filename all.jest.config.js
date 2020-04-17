a287f3ab2063d7b5975fb22fda958a1025a9baf2
Joost Zöllner <joost.zoellner@holisticon.de>
2020-04-17 16:38:18 +0200

    add dummy all project

---
+++ all.jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'all',
+  preset: './jest.config.js',
+  coverageDirectory: './coverage/all',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
+};
